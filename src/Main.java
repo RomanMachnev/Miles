a8f0a95272e0aa0b1f8413fcdc143fb5fe59cef5
Roman Machnev <machnev93@yandex.ru>
2021-03-10 08:34:08 +0300

    add attachment

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int ticket = 20000 ;
+        int other = 20 ;
+        int miles = ticket / other ;
+        System.out.println(miles);
+
+    }
 }
